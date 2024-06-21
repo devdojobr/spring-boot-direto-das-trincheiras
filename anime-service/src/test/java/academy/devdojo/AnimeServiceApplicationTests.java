4b01481b54c9aa596605249de641cb89e8a77adb
William Suane <william.suane@devdojo.academy>
2024-06-21 16:25:32 +0200

    video03: initial commit of the project

---
+++ anime-service/src/test/java/academy/devdojo/AnimeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.devdojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

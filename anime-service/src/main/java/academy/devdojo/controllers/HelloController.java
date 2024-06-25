f7cd0e253451b18e5704e9212b41a6fdc7f23d3c
William Suane <william.suane@devdojo.academy>
2024-06-25 11:28:57 +0200

    video06:GetMapping and RestController pt 01

---
+++ anime-service/src/main/java/academy/devdojo/controllers/HelloController.java
@@ -0,0 +1,13 @@
+package academy.devdojo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("hi")
+    public String hi() {
+        return "OMAE WA MOU SHINDE IRU";
+    }
+}

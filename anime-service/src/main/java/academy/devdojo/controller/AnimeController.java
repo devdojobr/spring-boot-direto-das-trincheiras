f1ccb40ec208e74027cfd40c200e021ec171bc45
William Suane <william.suane@devdojo.academy>
2024-06-30 10:29:02 +0200

    video10: Virtual Threads, Stacktrace e Tomcat Threads

--- anime-service/src/main/java/academy/devdojo/controller/AnimeController.java
+++ anime-service/src/main/java/academy/devdojo/controller/AnimeController.java
@@ -1,17 +1,22 @@
 package academy.devdojo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("v1/animes")
+@Slf4j
 public class AnimeController {
 
     @GetMapping
-    public List<String> listAll() {
+    public List<String> listAll() throws InterruptedException {
+        log.info(Thread.currentThread().getName());
+        TimeUnit.SECONDS.sleep(1);
         return List.of("Ninja Kamui", "Kaijuu-8gou");
     }
 }
